95d175e3daa3ed92c0f180a38eaa02c787554821
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-04-26 00:59:55 +0530

    update

--- client/app/(home)/awarding-section.jsx
+++ client/app/(home)/awarding-section.jsx
@@ -45,7 +45,7 @@ function Awarding() {
 
     return (
         <>
-            <div className='w-full lg:pb-10 pt-10 font-sans bg-white text-black px-[10vw] flex flex-col lg:flex-row items-start justify-start lg:justify-between lg:gap-32 gap-5 testimonial-section'>
+            <div className='w-full lg:pb-10 pt-10 font-sans bg-white text-black px-[10vw] lg:px-0 lg:pl-[10vw] flex flex-col lg:flex-row items-start justify-start lg:justify-between lg:gap-32 gap-5 testimonial-section'>
                 <div className='w-full flex items-start flex-col lg:h-full'>
                     <span className="text-[6vw] lg:text-[3vw] font-sans font-bold capitalize">
                         Award-winning
@@ -56,7 +56,7 @@ function Awarding() {
 
                     <div className="w-full bg-white mt-3 text-[#2E2E27] flex flex-col">
                         <div className="w-full flex flex-col gap-3">
-                            <span className="text-[18px] font-extralight">
+                            <span className="text-[18px] font-extralight capitalize">
                                 Read more about how our university's commitment to <br /> excellence has been recognised with our <a href="#" className="underline font-bold">latest <br /> awards.</a>
                             </span>
                         </div>
@@ -67,7 +67,7 @@ function Awarding() {
                         <div className="w-full bg-white hidden lg:block">
                             <div className="bg-white w-full pt-10 pb-8 flex flex-row justify-start overflow-x-auto gap-8 snap-x snap-mandatory scrollbar-hide scroll-smooth">
                                 {[...Array(6)].map((_, i) => (
-                                    <div key={i} className="w-56 h-56 bg-gray-500 snap-start shrink-0">
+                                    <div key={i} className="w-60 h-60 bg-gray-500 snap-start shrink-0">
                                         <img src="/Logo.svg" alt={`logo-${i}`} className="w-full h-full object-contain" />
                                     </div>
                                 ))}
--- client/app/(home)/nss-section.jsx
+++ client/app/(home)/nss-section.jsx
@@ -42,14 +42,14 @@ function NSS() {
 
     return (
         <>
-            <div className='w-full bg-[#2e2e27] px-[10vh] py-[10vh] p-2 nss-section grid lg:grid-cols-2'>
-                <div className='w-full flex justify-center items-center nss-left'>
-                    <span className='text-[18px] hover:underline font-sans text-center lg:text-start font-bold'>
+            <div className='w-full bg-[#2e2e27] px-[10vh] lg:px-[20vh] py-[10vh] p-2 nss-section flex flex-col lg:flex-row justify-center items-center lg:gap-32 capitalize'>
+                <div className='w-full lg:w-[70%] flex items-center nss-left'>
+                    <span className='text-[20px] hover:underline font-sans text-center lg:text-start font-bold'>
                         The National Student Survey (NSS) 2025: A UK-wide annual survey for final-year students to share their views and feedback on their time at University.
                     </span>
                 </div>
-                <div className='w-full flex justify-center items-center nss-right mt-5 lg:mt-0'>
-                    <img src="/Images/NSS-2.jpg" alt="NSS" className='w-[55%]' />
+                <div className='w-full lg:w-[30%] flex justify-center items-center nss-right mt-5 lg:mt-0'>
+                    <img src="/Images/NSS-2.jpg" alt="NSS" className='w-[80%] lg:w-full' />
                 </div>
             </div>
         </>
