a6ce4f981392877a10d7193cbfa214a073669816
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-04-29 16:44:01 +0530

    update

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,41 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { FaWhatsapp, FaTiktok, FaInstagram, FaYoutube, FaFacebookF, FaLinkedin } from "react-icons/fa";
 
 export const Footer = () => {
-    const BASE_MOBILE = process.env.NEXT_PUBLIC_BASE_MOBILE;
+
+    const FACEBOOK_LIKE = process.env.NEXT_PUBLIC_FB_LINK;
+    const TIKTOK_LINK = process.env.NEXT_PUBLIC_TIKTOK_LINK;
+    const INSTAGRAM_LINK = process.env.NEXT_PUBLIC_INSTAGRAM_LINK;
+    const YOUTUBE_LINK = process.env.NEXT_PUBLIC_YOUTUBE_LINK;
+
     return (
-        <>
-            <footer>
-                <div className='w-full hsrc flex font-sans bg-[#F7FAFF] text-black flex-col gap-3 justify-center items-center pt-5 capitalize'>
-                    <span className='text-[#D80E03] text-lg font-semibold'>Langford Collage</span>
-                    <div className='w-full md:w-[470px] text-center p-5'>
-                        <span className='text-base text-[#000000CC]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
-                    </div>
-                    <div className='w-full justify-center flex flex-wrap gap-5  mt-3 lg:mt-5 text-[16px]'>
-                        <a href='/'>Home</a>
-                        <a href='about'>About</a>
-                        <a href='contact_us'>Contact Us</a>
-                        <a href=''>Course</a>
-                        <a href=''>Individuals</a>
-                    </div>
-                    <div className='w-full justify-center flex flex-col lg:flex-row gap-3 lg:gap-10 text-[16px] mt-3 mb-5'>
-                        <div className='flex gap-1 items-center justify-center'>
-                            <img src="/Icon/location.png" alt="" className='w-4 h-4' />
-                            <a href='' target='_blank'>489, Pallama Road, Chillaw, Bangadeniya</a>
-                        </div>
-                        <div className='flex gap-1 items-center justify-center'>
-                            <img src="/Icon/call.png" alt="" className='w-4 h-4' />
-                            <a href={`tel:${BASE_MOBILE}`} target='_blank'>{BASE_MOBILE?.charAt(0) === '+' ? `0` + BASE_MOBILE.slice(3) : BASE_MOBILE}</a>
-                        </div>
-                        <div className='flex gap-1 items-center justify-center'>
-                            <img src="/Icon/email.png" alt="" className='w-4 h-4' />
-                            <a href='mailto:support@langfordcollage.com' target='_blank'>info@langfordcollage.com</a>
-                        </div>
-                    </div>
-                    <div className='w-full justify-center flex gap-5 text-[16px] bg-black p-3 py-4'>
-                        <span className='text-white'>Copyright © 2025 KDsolution All Rights Reserved.</span>
+        <footer className="bg-white text-black w-full h-[80vh] px-[10vw] capitalize">
+            <div className="w-full h-full mx-auto py-10 grid grid-cols-1 md:grid-cols-4 gap-8">
+                <div className="col-span-1 gap-3 flex flex-col">
+                    {/* <img src="/Logo.svg" alt="University of Chester" className="w-40 mb-4" /> */}
+                    <span className='text-3xl font-bold text-red-500'>Langford Collage</span>
+                    <p className="text-[18px] mt-3">University of Chester<br />Parkgate Road<br />Chester<br />CH1 4BJ</p>
+                    <div className="flex mt-4 space-x-5">
+                        <a href={`${FACEBOOK_LIKE}`}><FaFacebookF size={25} /></a>
+                        <a href={`${INSTAGRAM_LINK}`}><FaInstagram size={25} /></a>
+                        <a href={`${TIKTOK_LINK}`}><FaTiktok size={25} /></a>
+                        <a href={`${YOUTUBE_LINK}`}><FaYoutube size={25} /></a>
+                        <a href={`${FACEBOOK_LIKE}`}><FaLinkedin size={25} /></a>
                     </div>
                 </div>
-            </footer>
-        </>
-    )
-}
+
+                <div>
+                    <h4 className="font-semibold mb-3">Quick links</h4>
+                    <ul className="space-y-2 text-[18px]">
+                        <li><a href="/individual/how_will_you_learn">How will you learn</a></li>
+                        <li><a href="/individual/our_hubs">our hubs</a></li>
+                        <li><a href="/individual/top-up_degree">top up degree</a></li>
+                        <li><a href="/individual/blog">blog</a></li>
+                        <li><a href="/individual/testimonial">testimonial</a></li>
+                        <li><a href="/about">about us</a></li>
+                    </ul>
+                </div>
+
+                <div className='col-span-2 w-full'>
+                    <h4 className="font-semibold mb-3">About Us,</h4>
+                    <span className='text-[18px]'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, ea. Libero animi omnis sunt tenetur modi! Saepe aut fugiat pariatur repellendus accusamus quam tempore, eum est voluptas illo totam quod!
+                    </span>
+                </div>
+            </div>
+
+            <div className="bg-black text-white text-center py-4 text-sm">
+                <span>Copyright © 2025 KDsolution All Rights Reserved.</span>
+            </div>
+        </footer>
+    );
+};
