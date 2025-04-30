4182ee309fb4c73061ea8abd3d1bed384857be33
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-04-30 14:04:54 +0530

    update

--- client/app/(root)/individual/our_hubs/our-hubs-hero-section.jsx
+++ client/app/(root)/individual/our_hubs/our-hubs-hero-section.jsx
@@ -16,7 +16,7 @@ function OurHubsHero() {
     return (
         <>
             <div className='w-[100%] px-5 lg:px-32 fade-in'>
-                <div className='w-full h-[45vh] mb-[20vh] flex justify-center items-center flex-col gap-5 lg:pl-36'>
+                <div className='w-full h-[40vh] mb-[20vh] flex justify-center items-center flex-col gap-5 lg:pl-36'>
                     <div className='w-full flex flex-col text-center lg:text-start fade-in'>
                         <span className='font-bold text-5xl md:text-[6vw]'>Online Langford</span>
                     </div>
